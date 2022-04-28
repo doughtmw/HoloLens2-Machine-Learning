341a0f0a68b6832424b97b45478f664e77f3f431
Matt Wilchek <mwilchek@gmu.edu>
2022-04-28 17:22:37 -0400

    alt version of label rotation

--- Assets/Scripts/BoundingBoxScript.cs
+++ Assets/Scripts/BoundingBoxScript.cs
@@ -5,6 +5,7 @@ using TMPro;
 public class BoundingBoxScript : MonoBehaviour
 {
     public NetworkResultWithLocation networkResultWithLocation;
+    float rotationSpeed = 0.5f;
     TextMeshPro textComponent;
 
     void Start()
@@ -29,9 +30,20 @@ public class BoundingBoxScript : MonoBehaviour
 
     void Update()
     {
+        // Corbett
         //Rotate only the text box (the label of the bbox) to face the user 
-        Vector3 targetDirection = Camera.main.transform.position - textComponent.transform.position;
-        Quaternion rotation = Quaternion.LookRotation(targetDirection, Vector3.up);
-        transform.rotation = rotation;
+        //Vector3 targetDirection = Camera.main.transform.position - textComponent.transform.position;
+        //Quaternion rotation = Quaternion.LookRotation(targetDirection, Vector3.up);
+        //transform.rotation = rotation;
+
+        // Wilchek
+        //Rotate only the text box (the label of the bbox) to face the user
+        Quaternion targetRotation = Quaternion.LookRotation(Camera.main.transform.position - textComponent.transform.position);
+        // Setting Rotation along z axis to zero
+        targetRotation.z=0;
+        //Setting Rotation along x axis to zero
+        targetRotation.x=0;
+        transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, rotationSpeed * Time.deltaTime);
+
     }
 }

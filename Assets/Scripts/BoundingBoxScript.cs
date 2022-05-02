031a5c170eaa7b24c6adef09a70d08b21692d5ba
mattycorbett <87574595+mattycorbett@users.noreply.github.com>
2022-05-02 10:38:21 -0400

    Updated text box rotation

--- Assets/Scripts/BoundingBoxScript.cs
+++ Assets/Scripts/BoundingBoxScript.cs
@@ -32,18 +32,20 @@ public class BoundingBoxScript : MonoBehaviour
     {
         // Corbett
         //Rotate only the text box (the label of the bbox) to face the user 
-        //Vector3 targetDirection = Camera.main.transform.position - textComponent.transform.position;
-        //Quaternion rotation = Quaternion.LookRotation(targetDirection, Vector3.up);
-        //transform.rotation = rotation;
+        Vector3 targetDirection = textComponent.transform.position - Camera.main.transform.position;
+        Quaternion rotation = Quaternion.LookRotation(targetDirection, Vector3.up);
+        rotation.z = 0;
+        rotation.x = 0;
+        textComponent.transform.rotation = rotation;
 
         // Wilchek
         //Rotate only the text box (the label of the bbox) to face the user
-        Quaternion targetRotation = Quaternion.LookRotation(Camera.main.transform.position - textComponent.transform.position);
+        //Quaternion targetRotation = Quaternion.LookRotation(Camera.main.transform.position - textComponent.transform.position);
         // Setting Rotation along z axis to zero
-        targetRotation.z=0;
+        //targetRotation.z=0;
         //Setting Rotation along x axis to zero
-        targetRotation.x=0;
-        textComponent.transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, rotationSpeed * Time.deltaTime);
+        //targetRotation.x=0;
+        //textComponent.transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, rotationSpeed * Time.deltaTime);
 
     }
 }
